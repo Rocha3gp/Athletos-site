11ec2a8d51d295aeaa40e04f5e3ce737842a7e3a
Rocha3gp <rocha.exe@gmail.com>
2026-06-10 10:01:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
+
 fetch('products.json')
 .then(r=>r.json())
-.then(data=>{
- const grid=document.getElementById('grid');
- data.forEach(p=>{
-   grid.innerHTML+=`
-   <div class="card">
-     <h3>${p.nome}</h3>
-     <p>Categoria: ${p.categoria}</p>
-     <p>R$ ${p.preco}</p>
-     <a href="https://wa.me/5547999143085?text=Olá! Tenho interesse na camiseta ${encodeURIComponent(p.nome)}">
-       <button>Comprar</button>
-     </a>
-   </div>`;
- });
-});
+.then(products=>{
+const grid=document.getElementById('grid');
+products.forEach(p=>{
+grid.innerHTML+=`
+<div class="card">
+<h3>${p.name}</h3>
+<p>${p.desc}</p>
+<p><strong>${p.price}</strong></p>
+<button onclick="window.open('https://wa.me/5547999143085?text=Tenho interesse em ${p.name}')">Comprar</button>
+</div>`
+})
+})
